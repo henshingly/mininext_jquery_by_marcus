503009048c2c2568b5ecc91d03f2d6e76f62aa87
Henshingly <dietmar_kersting@web.de>
2024-08-25 15:22:45 +0200

    Update
    Adjustments for the LMO PHP 8. Readability of the source code improved. Small errors fixed

--- template/mini/mininext_all.tpl.php
+++ template/mini/mininext_all.tpl.php
@@ -1,23 +1,15 @@
-
 <table class="nextgame" border="0" width="155">
   <tr>
     <td>
       <table width="100%">
         <caption><!--gameTxt--></caption>
         <tr>
-         <th><!--gameDate--> <!--gameTime--></th>
+          <th><!--gameDate--> <!--gameTime--></th>
         </tr>
         <tr>
           <td align="center"><!--imgHomeBig--><br /><!--homeName--><br />-<br /><!--guestName--><br /><!--imgGuestBig-->
-</td>
+          </td>
         </tr>
-<!--        <tr>
-          <td align="center">&nbsp;-&nbsp;</td>
-        </tr>
-        <tr>
-          <td align="center"></td>
-        </tr>
--->        
         <tr>
           <td><small><!--gameNote-->&nbsp;</small></td>
         </tr>
@@ -30,44 +22,44 @@
         <tr>
           <th>  
             <table align="center" border="0" cellpadding="0" cellspacing="0">
-             	<tr>
+              <tr>
                 <th bgcolor="#009900" height="7" width="<!--winWidth-->"></th>
                 <th bgcolor="#999999" height="7" width="<!--drawWidth-->"></th>
                 <th bgcolor="#990000" height="7" width="<!--lostWidth-->"></th>
-             	</tr>
+              </tr>
             </table>
           </th>
         </tr>
         <tr>
           <td align="center">
             <ul>
-        <!-- BEGIN matches -->
-        <li class="<!--class-->"><!--date--> <!--time--> <!--hTore-->:<!--gTore--> (<!--where-->) <small><!--matchingName--></small></li>
-        <!-- END matches -->
-           </ul>
-         </td>
-       </tr>
-       <!--mehr-->
-      <!-- BEGIN mehrarchivspiele -->
+              <!-- BEGIN matches -->
+              <li class="<!--class-->"><!--date--> <!--time--> <!--hTore-->:<!--gTore--> (<!--where-->) <small><!--matchingName--></small></li>
+              <!-- END matches -->
+            </ul>
+          </td>
+        </tr>
+        <!--mehr-->
+        <!-- BEGIN mehrarchivspiele -->
         <tr id="allespiele">
           <td align="center">
             <ul>
-        <!-- BEGIN matches2 -->
-        <li class="<!--class-->"><!--date--> <!--time--> <!--hTore-->:<!--gTore--> (<!--where-->) <small><!--matchingName--></small></li>
-        <!-- END matches2 -->
-           </ul>
-         </td>
-       </tr>
-       <!-- END mehrarchivspiele -->
-       <tr>
-         <td align="right"><small><!--ligaDatum-->&nbsp;<!--copy--></small></td>
-       </tr>
+              <!-- BEGIN matches2 -->
+              <li class="<!--class-->"><!--date--> <!--time--> <!--hTore-->:<!--gTore--> (<!--where-->) <small><!--matchingName--></small></li>
+              <!-- END matches2 -->
+            </ul>
+          </td>
+        </tr>
+        <!-- END mehrarchivspiele -->
+        <tr>
+          <td align="right"><small><!--ligaDatum-->&nbsp;<!--copy--></small></td>
+        </tr>
       </table>
       <!-- BEGIN previous -->
       <table width="100%">
         <caption><!--previous_gameTxt--></caption>
         <tr>
-         <th><!--previous_gameDate--> <!--previous_gameTime--></th>
+          <th><!--previous_gameDate--> <!--previous_gameTime--></th>
         </tr>
         <tr>
           <td align="center"><!--previous_homeName--></td>
